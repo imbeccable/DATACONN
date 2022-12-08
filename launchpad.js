fd72fedceeb532d92529dc7cfeae157296bfa1be
Becca Nika <rnika3@uic.edu>
2022-12-08 13:05:59 -0600

    set controller keys to position and added in initial shape maker

--- launchpad.js
+++ launchpad.js
@@ -114,8 +114,19 @@ class rain {
     }
     
     display() {
-        fill(0,0,255);
-        circle(x,y,d)
+        // fill(0,0,255);
+        // circle(x,y,d);
+
+        var canvas = document.getElementById('circle');
+        if (canvas.getContext) {
+            var ctx = canvas.getContext('2d');
+            var R = 20;
+            ctx.beginPath();
+            ctx.arc(x, y, R, 0, 2 * Math.PI, false);
+            ctx.lineWidth = 3;
+            ctx.strokeStyle = '#FF0000';
+            ctx.stroke();
+        }
     }
 
     move() {
@@ -141,7 +152,6 @@ function waves(resp,note) {
             raindrops.push(new rain(i+5,80,dew/2,5))
         }
     }
-    console.log("got out")
 
     for (i=0;i<raindrops.length;i++) {
         raindrops[i].display();
@@ -172,135 +182,197 @@ function changeDisplay(resp){
 function setXnY(note) {
 
     if (note == 36) {
-        x = windowWidth/8;
-        y = windowHeight - windowHeight/8
+        x = 100;
+        y = 800;
     } else if (note == 37) {
-        x = windowWidth/7;
-        y = windowHeight - windowHeight/8        
+        x = 200;
+        y = 800;    
     } else if (note == 38) {
-        x = windowWidth/6;
-        y = windowHeight - windowHeight/8
+        x = 300;
+        y = 800;
     } else if (note == 39) {
-        x = windowWidth/5;
-        y = windowHeight - windowHeight/8
+        x = 400;
+        y = 800;
     } else if (note == 40) {
-        
+        x = 100;
+        y = 700;
     } else if (note == 41) {
-        
+        x = 200;
+        y = 700;
     } else if (note == 42) {
-        
+        x = 300;
+        y = 700;
     } else if (note == 43) {
-        
+        x = 400;
+        y = 700;
     } else if (note == 44) {
-        
+        x = 100;
+        y = 600;
     } else if (note == 45) {
-        
+        x = 200;
+        y = 600;
     } else if (note == 46) {
-        
+        x = 300;
+        y = 600;
     } else if (note == 47) {
-        
+        x = 400;
+        y = 600;
     } else if (note == 48) {
-        
+        x = 100;
+        y = 500;
     } else if (note == 49) {
-        
+        x = 200;
+        y = 500;
     } else if (note == 50) {
-        
+        x = 300;
+        y = 500;
     } else if (note == 51) {
-        
+        x = 400;
+        y = 500;
     } else if (note == 52) {
-        
+        x = 100;
+        y = 400;
     } else if (note == 53) {
-        
+        x = 200;
+        y = 400;
     } else if (note == 54) {
-        
+        x = 300;
+        y = 400;
     } else if (note == 55) {
-        
+        x = 400;
+        y = 400;
     } else if (note == 56) {
-        
+        x = 100;
+        y = 300;
     } else if (note == 57) {
-        
+        x = 200;
+        y = 300;
     } else if (note == 58) {
-        
+        x = 300;
+        y = 300;
     } else if (note == 59) {
-        
+        x = 400;
+        y = 300;
     } else if (note == 60) {
-        
+        x = 100;
+        y = 200;
     } else if (note == 61) {
-        
+        x = 200;
+        y = 200;
     } else if (note == 62) {
-        
+        x = 300;
+        y = 200;
     } else if (note == 63) {
-        
+        x = 400;
+        y = 200;
     } else if (note == 64) {
-        
+        x = 100;
+        y = 100;
     } else if (note == 65) {
-        
+        x = 200;
+        y = 100;
     } else if (note == 66) {
-        
+        x = 300;
+        y = 100;
     } else if (note == 67) {
-        
+        x = 400;
+        y = 100;
     } else if (note == 68) {
-        
+        x = 500;
+        y = 800;
     } else if (note == 69) {
-        
+        x = 600;
+        y = 800;
     } else if (note == 70) {
-        
+        x = 700;
+        y = 800;
     } else if (note == 71) {
-        
+        x = 800;
+        y = 800;
     } else if (note == 72) {
-        
+        x = 500;
+        y = 700;
     } else if (note == 73) {
-        
+        x = 600;
+        y = 700;
     } else if (note == 74) {
-        
+        x = 700;
+        y = 700;
     } else if (note == 75) {
-        
+        x = 800;
+        y = 700;
     } else if (note == 76) {
-        
+        x = 500;
+        y = 600;
     } else if (note == 77) {
-        
+        x = 600;
+        y = 600;
     } else if (note == 78) {
-        
+        x = 700;
+        y = 600;
     } else if (note == 79) {
-        
+        x = 800;
+        y = 600;
     } else if (note == 80) {
-        
+        x = 500;
+        y = 500;
     } else if (note == 81) {
-        
+        x = 600;
+        y = 500;
     } else if (note == 82) {
-        
+        x = 700;
+        y = 500;
     } else if (note == 83) {
-        
+        x = 800;
+        y = 500;
     } else if (note == 84) {
-        
+        x = 500;
+        y = 400;
     } else if (note == 85) {
-        
+        x = 600;
+        y = 400;
     } else if (note == 86) {
-        
+        x = 700;
+        y = 400;
     } else if (note == 87) {
-        
+        x = 800;
+        y = 400;
+    } else if (note == 88) {
+        x = 500;
+        y = 300;
     } else if (note == 89) {
-        
+        x = 600;
+        y = 300;
     } else if (note == 90) {
-        
+        x = 700;
+        y = 300;
     } else if (note == 91) {
-        
+        x = 800;
+        y = 300;
     } else if (note == 92) {
-        
+        x = 500;
+        y = 200;
     } else if (note == 93) {
-        
+        x = 600;
+        y = 200;
     } else if (note == 94) {
-        
+        x = 700;
+        y = 200;
     } else if (note == 95) {
-        
+        x = 800;
+        y = 200;
     } else if (note == 96) {
-        
+        x = 500;
+        y = 100;
     } else if (note == 97) {
-        
+        x = 600;
+        y = 100;
     } else if (note == 98) {
-        
+        x = 700;
+        y = 100;
     } else {
-        
+        x = 800;
+        y = 100;
     } 
 }
 
